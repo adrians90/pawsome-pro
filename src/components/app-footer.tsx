fa493d7b4244349c5b2a2d842c39198015c6b4e2
adrians90 <adriansandu9090@gmail.com>
2024-06-10 13:04:30 +0100

    Position the footer at the bottom of the page

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,6 +1,6 @@
 export default function AppFooter() {
   return (
-    <footer className="border-t border-black/5 py-5">
+    <footer className="mt-auto border-t border-black/5 py-5">
       <small className="opacity-50">&copy; 2024 Adrian Sandu</small>
     </footer>
   );
