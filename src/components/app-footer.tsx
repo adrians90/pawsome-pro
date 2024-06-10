876d3602ef81aaa4160eab4ac2b76a61d78f1f98
adrians90 <adriansandu9090@gmail.com>
2024-06-10 13:03:08 +0100

    style the footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,3 +1,7 @@
 export default function AppFooter() {
-  return <div>app-footer</div>;
+  return (
+    <footer className="border-t border-black/5 py-5">
+      <small className="opacity-50">&copy; 2024 Adrian Sandu</small>
+    </footer>
+  );
 }
