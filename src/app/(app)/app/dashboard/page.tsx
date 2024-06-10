98797d3c11483681790e13bc19312d1e2816736f
adrians90 <adriansandu9090@gmail.com>
2024-06-10 13:28:09 +0100

    Clean up the dashboard/page component.

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,19 +1,13 @@
+import Branding from "@/components/branding";
+
+import Stats from "@/components/stats";
+
 export default function Page() {
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
-        <section>
-          <h1 className="font-medium text-2xl leading-6">
-            Pawsome <span className="font-semibold">Pro</span>
-          </h1>
-          <p className="text-lg opacity-80">
-            Manage your pet daycare with ease.
-          </p>
-        </section>
-        <section className="text-center">
-          <p className="text-2xl font-bold leading-6">2</p>
-          <p className="opacity-80">Current guests.</p>
-        </section>
+        <Branding />
+        <Stats />
       </div>
     </main>
   );
