a420c5cf14bdfd78a7b7d946cb8d89124d4a7e3f
adrians90 <adriansandu9090@gmail.com>
2024-06-10 10:27:22 +0100

    Add image on main page & domain name to next config file

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of Pawsome Pro"
+        width={519}
+        height={472}
+      />
+
+      <div></div>
+    </main>
+  );
 }
