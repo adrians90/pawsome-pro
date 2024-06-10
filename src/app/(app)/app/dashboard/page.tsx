fd2d4bf7ffbbe0e2ca0cb3ad2c87f5a9a4c61e53
adrians90 <adriansandu9090@gmail.com>
2024-06-10 13:14:43 +0100

    Style the dashboard page

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,20 @@
 export default function Page() {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <section>
+          <h1 className="font-medium text-2xl leading-6">
+            Pawsome <span className="font-semibold">Pro</span>
+          </h1>
+          <p className="text-lg opacity-80">
+            Manage your pet daycare with ease.
+          </p>
+        </section>
+        <section className="text-center">
+          <p className="text-2xl font-bold leading-6">2</p>
+          <p className="opacity-80">Current guests.</p>
+        </section>
+      </div>
+    </main>
+  );
 }
