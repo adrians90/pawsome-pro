244c77dc13276af35265d6f9425fcc0bfe680657
adrians90 <adriansandu9090@gmail.com>
2024-06-11 14:11:41 +0100

    Update Account page with customized H1 and ContentBlock components

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -4,8 +4,9 @@ import H1 from "@/components/h1";
 export default function Page() {
   return (
     <main>
-      <H1>Your Account</H1>
-      <ContentBlock>
+      <H1 className="my-8 text-white">Your Account</H1>
+
+      <ContentBlock className="h-[500px] flex justify-center items-center">
         <p>Logged in as...</p>
       </ContentBlock>
     </main>
