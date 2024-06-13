f32424b27af4e0eb1958ac1278407c6cece86fd2
adrians90 <adriansandu9090@gmail.com>
2024-06-13 11:07:06 +0100

    Error State (sonner toast)

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
-import { Button } from "./ui/button";
+
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { addPet } from "@/actions/actions";
 import PetFormBtn from "./pet-form-btn";
+import { toast } from "sonner";
 
 type PetFormProps = {
   actionType: "add" | "edit";
@@ -22,7 +23,11 @@ export default function PetForm({
   return (
     <form
       action={async (formData) => {
-        await addPet(formData);
+        const error = await addPet(formData);
+        if (error) {
+          toast.warning(error.message);
+          return;
+        }
         onFormSubmission();
       }}
       className="flex flex-col"
