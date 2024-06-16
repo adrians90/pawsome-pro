be63647be9d9f36c3440cb5a483b13f34a567e46
adrians90 <adriansandu9090@gmail.com>
2024-06-16 12:29:09 +0100

    Implement next auth for authentication

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,13 +1,22 @@
 "use server";
 
+import { signIn } from "@/lib/auth";
 import prisma from "@/lib/db";
-import { PetEssentials } from "@/lib/types";
 
 import { sleep } from "@/lib/utils";
 import { petFormSchema, petIdSchema } from "@/lib/validations";
-import { Pet } from "@prisma/client";
+
 import { revalidatePath } from "next/cache";
 
+//--- USER ACTIONS
+
+export async function logIn(formData: FormData) {
+  const authData = Object.fromEntries(formData.entries());
+
+  await signIn("credentials", authData);
+}
+
+// --- PET ACTIONS
 export async function addPet(pet: unknown) {
   await sleep(1000);
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,44 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./db";
+import bcrypt from "bcrypt";
 
 const config = {
   pages: {
     signIn: "/login",
   },
 
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        //runs on login
+
+        const { email, password } = credentials;
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log("No user found");
+          return null;
+        }
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+        if (!passwordsMatch) {
+          console.log("Invalid credentials");
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
+      //runs on every request with middleware
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
       if (isTryingToAccessApp) {
         return false;
@@ -18,4 +49,4 @@ const config = {
   },
 } satisfies NextAuthConfig;
 
-export const { auth } = NextAuth(config);
+export const { auth, signIn } = NextAuth(config);
