606f0fe1647cfa0eb6a3f35a1e913a733cbd852e
adrians90 <adriansandu9090@gmail.com>
2024-06-17 10:27:03 +0100

    Next auth route handlers ( GET , POST

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { auth, signIn, signOut } from "@/lib/auth";
+import { signIn, signOut } from "@/lib/auth";
 import prisma from "@/lib/db";
 
 import { sleep } from "@/lib/utils";
